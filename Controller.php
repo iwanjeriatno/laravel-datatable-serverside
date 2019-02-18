739be030ae0d4583a47fae38e81704325136ad49
Iwan Jeriatno <jeriat01@gmail.com>
2019-02-18 18:28:04 +0800

    controller

---
+++ Controller.php
@@ -0,0 +1,18 @@
+    public function datatable()
+    {
+        $data = PayrollItem::all();
+
+        return DataTables::of($data)
+        ->addColumn('action', function ($data) {
+                return  '<div class="dropdown">
+                            <a href="#" class="btn m-btn m-btn--hover-danger m-btn--icon m-btn--icon-only m-btn--pill" data-toggle="dropdown">
+                                <i class="la la-ellipsis-h"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right">
+                               <a class="dropdown-item" href=""><i class="la la-apps"></i> Edit</a>
+                            </div>
+                        </div>';
+            })
+        ->editColumn('id', '{{$id}}')
+        ->make(true);
+    }
