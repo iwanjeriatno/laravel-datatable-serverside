bc155a1c63e21f6129a4c0b4ae5a74423dfd3da1
Iwan Jeriatno <jeriat01@gmail.com>
2019-02-18 18:24:22 +0800

    index

---
+++ index.blade.php
@@ -0,0 +1,18 @@
+<div class="table-responsive">
+    <table class="table table-striped- table-bordered table-hover table-checkable id="m_table_1" data-url="{{ route('komponen-gaji.datatable') }}">
+    	<thead class="text-center">
+    		<tr>
+    			<th width="1%">No</th>
+          <th>Nama Komponen</th>
+          <th>Deskripsi</th>
+          <th>Kategori</th>
+          <th>Sub Kategori</th>
+          <th>Kode Akun</th>
+          <th>Jumlah (Rp)</th>
+          <th>Default</th>
+          <th class="text-center" width="10%">Aksi</th>
+    		</tr>
+    	</thead>
+    	<tbody class="text-left"></tbody>
+    </table>
+</div>
